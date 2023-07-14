805ccdfaedbb222ac1db2b721a80d0773bfb3646
Brian Kimbrow <bkimbrow25@gmail.com>
2023-07-14 04:26:24 -0500

    update

--- quiz-app/src/Quiz.jsx
+++ quiz-app/src/Quiz.jsx
@@ -11,6 +11,8 @@ const Quiz = ({ questions  }) => {
             <span className="active-question-no">{currentQuestion + 1}</span>
             <span className="total-question">/{questions.length}</span>
             <h2>{question}</h2>
+            
+
         </>
         </div>
 
